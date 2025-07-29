6e7a255f89ca20556d42aaa1a3766a06116f7302
Chris Najman <christopher.najman@gmail.com>
2025-07-29 18:16:06 +0100

    Moved title to own field in json. Styling tweaks.

--- js-modules/pagination/components/paginator/helpers/load-page-content.js
+++ js-modules/pagination/components/paginator/helpers/load-page-content.js
@@ -1,9 +1,16 @@
 import { pageContent } from "../../../../globals.js"
 import announceLiveRegion from "./announce-live-region.js"
 
+const pageTitle = document.getElementById("page-title")
+const mainHeading = document.createElement("h1")
+
 export default function loadPageContent(allPages, pageNum) {
   const page = allPages.find((p) => p.id == pageNum.toString())
+
   if (page) {
+    mainHeading.textContent = page.title
+    pageTitle.innerHTML = "" // Clear any previous headings
+    pageTitle.append(mainHeading)
     pageContent.innerHTML = page.content
     announceLiveRegion(
       page.title
